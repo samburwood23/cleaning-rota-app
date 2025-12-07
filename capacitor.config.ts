bded3cd1484e63203ffc24f7cdcfd68464a9bf48
Samantha ( Sam) Burwood <152337271+samburwood23@users.noreply.github.com>
2025-12-07 11:53:32 +0000

    Update appId in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.cleaningrota.app',
+  appId: 'com.cleaningrotaBurwood.app',
   appName: 'Cleaning Rota',
   webDir: 'dist',
   server: {
